fc46ac2a6d8c57df330963ffaabc57c3647a3d2c
Nathan Whitmore <nathan@reproducible.co.nz>
2021-04-19 12:25:09 +1200

    fixes

---
+++ R/supervised.R
@@ -0,0 +1,204 @@
+
+supervised <- function(seed = TRUE, shrink = 10){
+
+  if(seed == TRUE) {set.seed(27)} else {set.seed(NULL)}
+
+
+  # find folder
+  dir <- choose.dir(default = "", caption = "Select folder which contains DJI drone imagery\nNote: only select the exterior folder")
+
+  # set path
+  path <- paste0(dir, "\\map\\")
+
+  # output name
+  output.name <- sub(".*\\\\", "", dir)
+
+  # finding training file
+  training.file <- choose.files(default = "", caption = "Select the ESRI shapefile (.shp) that will be used for training")
+
+  # output folder
+  out.dir <- choose.dir(default = "", caption = "Select folder where the output files are to be put")
+
+  # spectral
+  blue <- raster(paste0(path , "result_Blue.tif"))
+  green <- raster(paste0(path , "result_Green.tif"))
+  red <- raster(paste0(path , "result_Red.tif"))
+  red_edge <- raster(paste0(path , "result_RedEdge.tif"))
+  nir <- raster(paste0(path , "result_NIR.tif"))
+
+  # derived
+  ndvi <- raster(paste0(path , "\\index_map\\", "NDVI.tif"))
+  gndvi <- raster(paste0(path , "\\index_map\\", "GNDVI.tif"))
+  lci <- raster(paste0(path , "\\index_map\\", "LCI.tif"))
+  ndre <- raster(paste0(path , "\\index_map\\", "NDRE.tif"))
+  osavi <- raster(paste0(path , "\\index_map\\", "OSAVI.tif"))
+
+  message("Step 1: raster stacking starting")
+
+  # make 3-d brick all (3 minutes)
+  region.brick <- brick(blue, green, red, red_edge, nir, ndvi, gndvi, lci, ndre, osavi)
+
+  message("Step 2: raster shrinking starting")
+
+  # make lower resolution raster (10 x smaller) 12 minutes with fun = median
+  region.brick <- aggregate(region.brick, fact = shrink, fun = median)
+
+  # read files
+  supervised <- st_read(training.file)
+
+  # add transform into longlat
+  supervised <- st_transform(supervised, crs = "+proj=longlat")
+
+  # add id
+  supervised$id <- 1:nrow(supervised)
+
+  # make count (IMPORTANT)
+  supervised$code <- as.numeric(as.factor(supervised$VegType))
+
+  # change to spatial type
+  supervised <- as_Spatial(supervised)
+
+  # mask
+  masked.brick <- mask(region.brick, supervised, sp = TRUE)
+
+  # Transform non-NA values to 1
+  counter <- masked.brick
+  counter[!is.na(counter)] <- 1
+  cellStats(counter, stat = "sum")
+
+  # unique vege names and number
+  vege.names.no <- length(unique(supervised$code))
+  vege.types <- sort(unique(supervised$VegType))
+
+
+  # for loop to extract as separate layers
+  ground_truth <- NULL
+  my.extract <- NULL
+
+  message("Step 3: extracting training layers")
+
+  for (i in 1:vege.names.no)  {
+    # get correct layer
+    my.mask <- supervised[supervised$code == i,]
+    masked <- raster::mask(region.brick, my.mask)
+
+    # get sample points (fixed at 100)
+    ground_truth <- as.data.frame(sampleRandom(masked, 200, xy= TRUE))
+    ground_truth$vege <- as.character(vege.types[i])
+
+    # rbind to original
+    my.extract <- rbind(ground_truth , my.extract)
+  }
+
+  # change to factor vege.types
+  levels(my.extract$vege) <- factor(vege.types)
+
+  message("Step 4: undertaking PCA")
+
+  # run pca to get non-correlated variables
+  pca <- princomp(my.extract[,-c(1,2,length(my.extract))])
+
+  # pca data frame
+  pca.df <- cbind(pca$scores, data.frame(vege_type = my.extract[,length(my.extract)]))
+
+  message("Step 5: undertaking random forests")
+
+  # randomforest
+  cross.val <- trainControl(method = "cv", number = 5)
+  train.rf <- train(vege_type ~ .,
+                    data = pca.df,
+                    method = "rf",
+                    trControl = cross.val)
+
+  # confusion matrix
+  cm <- confusionMatrix(train.rf)
+
+  # confusion matrix output
+
+
+  df.con <- as.data.frame(cm$table)
+  wide.con <- df.con %>% pivot_wider(names_from = Reference, values_from = Freq)
+  diag <- diag(as.matrix(wide.con[,-1]))
+
+  wide.con <- wide.con %>% adorn_totals()
+
+  last.line <- tail(wide.con,1)
+  last.line <- last.line[,- 1]
+
+  perfect <- diag/ last.line
+
+  # make list
+  output <- list()
+
+  output$random.forest <- train.rf
+  output$confusion.matrix <- cm
+  output$perfect <- perfect
+  output$imbalance <- last.line /max(last.line)
+
+  print(output)
+
+  # save outpu
+  capture.output(output, file = paste0(out.dir, "\\", output.name,"_supervised", ".txt"), append = FALSE)
+
+
+  # make predictions based on pca
+  pca.df$test <- predict(train.rf,  pca.df)
+
+
+  # PCA on entire raster
+  pca.raster <- rasterPCA(region.brick, nSamples = 1000)
+
+  pca.raster.df <- as.data.frame(pca.raster$map, xy = TRUE)
+  pca.raster.df <- na.omit(pca.raster.df)
+
+  # rename
+  colnames(pca.raster.df) <- c("x", "y", paste0("Comp.", 1:ncol(pca$scores)))
+
+  # prediction
+  pred.values <- predict(train.rf,  pca.raster.df)
+
+  # make new df
+  my.data <- pca.raster.df
+
+  # layer names
+  my.data$vege_pred <- pred.values
+
+  # change to raster
+
+  # change data to numeric
+  my.data$vege <- as.numeric(my.data$vege_pred)
+
+
+  # change df to raster
+  my.data <- my.data[, c(1,2,length(my.data))]
+
+  # make into raster
+  dfr <- rasterFromXYZ(my.data)  #Convert first two columns as lon-lat and third as value
+  crs(dfr) <- crs(region.brick)
+
+  # make into factor
+  dfr <- as.factor(dfr)
+
+
+  # make table to describe factors
+  x <- as.data.frame(levels(dfr))
+  x$vege_type <- vege.types
+  levels(dfr) <- x
+
+  message("Step 6: writing polygons")
+
+  # change to polygon
+  orig.poly <- rasterToPolygons(dfr, na.rm=TRUE, dissolve=TRUE)
+
+  decrumbed <- drop_crumbs(orig.poly, set_units(1, m^2))
+
+  # change to sf for easier handling
+  vege.sf <- st_as_sf(decrumbed)
+
+  # add actual levels to sf as these have been lost
+  vege.sf$vege_type <- vege.types
+
+  # write to shp for QGIS
+  st_write(vege.sf, paste0(out.dir, "\\", output.name,"_supervised",".shp"), driver = "ESRI Shapefile", append=FALSE)
+
+}
---
+++ R/unsupervised.R
@@ -0,0 +1,204 @@
+
+
+unsupervised <- function(type, super = 200, clusters, shrink = 10){
+
+  set.seed(27)
+
+
+  # find folder
+  dir <- choose.dir(default = "", caption = "Select folder which contains DJI drone imagery\nNote: only select the exterior folder")
+
+  # output name
+  output.name <- sub(".*\\\\", "", dir)
+
+  # output folder
+  out.dir <- choose.dir(default = "", caption = "Select folder where the output files are to be put")
+
+  # set path
+  path <- paste0(dir, "\\map\\")
+
+  # spectral
+  blue <- raster(paste0(path , "result_Blue.tif"))
+  green <- raster(paste0(path , "result_Green.tif"))
+  red <- raster(paste0(path , "result_Red.tif"))
+  red_edge <- raster(paste0(path , "result_RedEdge.tif"))
+  nir <- raster(paste0(path , "result_NIR.tif"))
+
+  # derived
+  ndvi <- raster(paste0(path , "\\index_map\\", "NDVI.tif"))
+  gndvi <- raster(paste0(path , "\\index_map\\", "GNDVI.tif"))
+  lci <- raster(paste0(path , "\\index_map\\", "LCI.tif"))
+  ndre <- raster(paste0(path , "\\index_map\\", "NDRE.tif"))
+  osavi <- raster(paste0(path , "\\index_map\\", "OSAVI.tif"))
+
+  message("Step 1: raster stacking starting")
+
+  if(type == "visible"){
+    region.brick <- brick(blue, green, red)
+
+  } else if (type == "false_colour_nir"){
+    region.brick <- brick(green, red, nir)
+
+  } else if (type == "ndvi"){
+    region.brick <- brick(ndvi, ndvi, ndvi)
+
+  } else if (type == "gndvi"){
+    region.brick <- brick(gndvi, gndvi, gndvi)
+
+  } else if (type == "lci"){
+    region.brick <- brick(lci, lci, lci)
+
+  } else if (type == "ndre"){
+    region.brick <- brick(ndre, ndre, ndre)
+
+  } else if (type == "osavi"){
+    region.brick <- brick(osavi, osavi, osavi)
+
+  } else {
+    message(paste0("Type '", type, "' is not available as an option"))
+  }
+
+  message("Step 2: raster shrinking starting")
+
+  # shrink resolution
+  region.brick <- aggregate(region.brick, fact = shrink)
+
+  # necessary for writing image to disk
+  nrows <- region.brick@nrows
+  ncols <- region.brick@ncols
+
+  # write to disk
+  long.name <- paste0(out.dir,"\\", output.name,"_", "unsupervised_", type, ".jpg")
+  jpeg(long.name, width = ncols, height = nrows)
+  plotRGB(region.brick, r = 3, g = 2, b = 1, stretch = "lin")
+  dev.off()
+
+  # read false colour
+  my.image <- readImage(long.name)
+
+
+  # super pixel calculator
+  tot.cells <- nrows * ncols
+  non.na <- region.brick[[1]]
+  non.na[!is.na(non.na)] <- 1
+  not.na <- freq(non.na)[1,2]
+  conv.factor <- not.na/tot.cells
+
+
+  message("Step 3: image segmentation starting")
+
+  Region.slic <- superpixels(input_image = my.image,
+                             method = "slic",
+                             superpixel = super/conv.factor,
+                             return_slic_data = TRUE,
+                             return_labels = TRUE, write_slic = "",
+                             verbose = FALSE)
+
+  imageShow(Region.slic$slic_data)
+
+  message("Step 4: affinity propagation starting")
+
+  # actual segmentation using Affinity Propagation
+  init <- Image_Segmentation$new()
+
+  Region.seg <- init$spixel_segmentation(input_image = my.image,
+                                         superpixel = super/conv.factor,
+                                         AP_data = TRUE,
+                                         use_median = TRUE,
+                                         sim_wL = 3,
+                                         sim_wA = 10,
+                                         sim_wB = 10,
+                                         sim_color_radius = 10,
+                                         verbose = TRUE)
+
+
+  imageShow(Region.seg$AP_image_data)
+
+
+
+  # change image array to to raster
+  sr.1 <- setValues(region.brick, Region.seg$AP_image_data)
+
+
+  # change to mono colour
+  mono.colour <- rgb_2gray(Region.seg$AP_image_data)
+
+
+  # change to raster
+  mon.rast <- raster(mono.colour)
+  crs(mon.rast) <- crs(sr.1)
+  extent(mon.rast) <- extent(sr.1)
+
+  message("Step 5: k-means clustering starting")
+
+  # kmeans clustering from raster
+  vege.df <- as.data.frame(sr.1)
+
+  if(clusters > length(unique(mon.rast))){
+    clusters <- length(unique(mon.rast))
+  } else   {
+    clusters <- clusters
+  }
+
+  vege.kmeans <- kmeans(vege.df, clusters)
+  k.clusters <- setValues(mon.rast, vege.kmeans$cluster)
+
+
+  # elbow method
+  set.seed(123)
+  # function to compute total within-cluster sum of square
+  wss <- function(k) {
+    kmeans(vege.df, k, nstart = 10 )$tot.withinss
+  }
+
+  # Compute and plot wss for k = 1 to max unique clusters
+  k.values <- 2:length(unique(mon.rast))
+
+  # extract wss for 2-15 clusters
+  wss_values <- map_dbl(k.values, wss)
+
+  elbow.title <- paste0(output.name,"_", type, "_elbow.png")
+
+  elbow.graph <- ggplot()+
+    geom_line(aes(x=k.values, y=wss_values))+
+    geom_point(aes(x=k.values, y=wss_values))+
+    scale_x_continuous(breaks = 1:15)+
+    labs(x="\nNumber of clusters",
+         y="Total within-clusters sum of squares\n",
+         title = paste0(output.name,"_", type, "\nClustering check using elbow method"))
+
+  ggsave(elbow.graph, filename = paste0(out.dir,"\\",  output.name,"_", "_unsupervised_", type, "_elbow.png"))
+
+
+  message("Step 6: polygon writing")
+
+  # mask sr.1
+  final <- mask(k.clusters, region.brick[[1]])
+
+  # change to polygon
+  orig.poly <- rasterToPolygons(final, na.rm=TRUE, dissolve = TRUE)
+
+  # change to sf for easier handling
+  vege.sf <- st_as_sf(orig.poly)
+  vege.sf$ID <- 1:nrow(vege.sf)
+
+  # assign vege.sf to superpixels
+  pix.raster <- raster(Region.slic$labels)
+  crs(pix.raster) <- crs(sr.1)
+  extent(pix.raster) <- extent(sr.1)
+
+  # super pixel rendering
+  pix.poly <- rasterToPolygons(pix.raster, na.rm=TRUE, dissolve = TRUE)
+  pix.sf <- st_as_sf(pix.poly)
+
+
+
+  # write to shp for QGIS
+  st_write(vege.sf, paste0(out.dir, "\\", output.name, "_unsupervised_", type,"_clusters", ".shp"),
+           driver = "ESRI Shapefile", append =FALSE)
+  st_write(pix.sf, paste0(out.dir, "\\", output.name, "_unsupervised_", type,"_superpixel", ".shp"),
+           driver = "ESRI Shapefile", append =FALSE)
+
+}
+
+
